8c81391e926fa87d3e94223cd111a438a7888a9c
Ambrosia <ambrosia@hana.re>
2015-12-26 15:42:07 +0000

    ReadToken.Header: add documentation

--- lib/plug_bare_token/read_token/header.ex
+++ lib/plug_bare_token/read_token/header.ex
@@ -1,9 +1,29 @@
 defmodule PlugBareToken.ReadToken.Header do
-  @default_into :token
+  @moduledoc """
+  A plug for reading tokens from HTTP headers.
+
+  ## Example using defaults
+      plug PlugBareToken.ReadToken.Header
+  This reads the string found in the first HTTP header named "authorization"
+  and stores it in the conn assigns as ```:token```
+  (e.g. ```conn.assigns[:token]```).
+  If no "authorization" header is found, ```nil``` will be in the assigns instead.
+
+  ## Example
+      plug PlugBareToken.ReadToken.Header, assign: :auth_token, header: "x-authorization", prefix: "Bearer "
+  This reads the string found in the first HTTP header named "x-authorization".
+  The token is extracted from the string that starts with "Bearer " and
+  stores it in the conn assigns as ```:auth_token```
+  (e.g. ```conn.assigns[:auth_token]```).
+  If the string found does not start with "Bearer ", ```nil``` will be found in
+  the assigns instead.
+  """
+
   @default_assign :token
   @default_header "authorization"
   @default_prefix :none
 
+  @doc false
   def init(options) do
     %{
       assign: Keyword.get(options, :assign, @default_assign),
@@ -12,6 +32,7 @@ defmodule PlugBareToken.ReadToken.Header do
     }
   end
 
+  @doc false
   def call(conn, opts) do
     token = conn
     |> Plug.Conn.get_req_header(opts.header)
@@ -20,15 +41,15 @@ defmodule PlugBareToken.ReadToken.Header do
     store_token(conn, token, opts)
   end
 
-  def find_token([], _opts), do: nil
+  defp find_token([], _opts), do: nil
 
-  def find_token(list, opts) when is_list(list) do
+  defp find_token(list, opts) when is_list(list) do
     hd(list) |> extract_token(opts)
   end
 
-  def extract_token(token, %{prefix: :none}), do: token
+  defp extract_token(token, %{prefix: :none}), do: token
 
-  def extract_token(string, %{prefix: prefix}) do
+  defp extract_token(string, %{prefix: prefix}) do
     cond do
       String.starts_with?(string, prefix) ->
         prefix_byte_size = byte_size(prefix)
@@ -38,9 +59,9 @@ defmodule PlugBareToken.ReadToken.Header do
         nil
     end
   end
-  def extract_token(_, _), do: :nil
+  defp extract_token(_, _), do: :nil
 
-  def store_token(conn, token, %{assign: assign}) do
+  defp store_token(conn, token, %{assign: assign}) do
     Plug.Conn.assign(conn, assign, token)
   end
 end
